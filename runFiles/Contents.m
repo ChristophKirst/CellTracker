a8e1cb9489e714f3ab4b62b4e81dcd3c2a718cc1
ChristophKirst <ckirst@nld.ds.mpg.de>
2014-01-17 11:35:11 -0500

    sync

---
+++ runFiles/Contents.m
@@ -0,0 +1,21 @@
+% RUNFILES
+%
+% Files
+%   HKloadLif                   - HKloadLif - Load Leica Image File Format
+%   mkPlots                     - 
+%   oneLSMSegmentCells          - 
+%   oneLSMSegmentCells2         - 
+%   runForFluffles              - 
+%   runFullTile_old             - For a set of tiled images, runs segmentCells (uses parfor for this), runs
+%   runMultipleCytoo            - 
+%   runOne                      - 
+%   runOneFluff                 - 
+%   runSegmentCells             - runSegmentCells(direc,outfile,nframes,nucstring,smadstring,paramfile)
+%   runSegmentCells_EN_Fluffles - runSegmentCells(direc,outfile,nframes,nucstring,smadstring,paramfile)
+%   runSegmentCells_old         - function runSegmentCells(direc,outfile,nframes,...
+%   runSegmentCellsLSMZstack    - 
+%   runSegmentCellsTile         - function runSegmentCells(direc,outfile,nframes,...
+%   runSegmentCellsTilePar      - function runSegmentCells(direc,outfile,nframes,...
+%   runTile                     - 
+%   runTileLoop                 - 
+%   runTracker                  - function runTrackerCCC(inmatfile,paramfile)
